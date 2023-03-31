08226c92da4ec8e6f499241d090d3e182ceb10e1
BhanuKiranG <bhanukiran5102@gmail.com>
2023-03-31 22:23:05 +0530

    EDA and problem statements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        [req.replace("\n","") for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     return requirements
--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logger
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -14,6 +14,6 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
-        return logger.logging.error(self.error_message)
+        return logging.error(self.error_message)
 
        
