6098538272fcad875d4e361a6476a8d87dd3fbcd
BhanuKiranG <bhanukiran5102@gmail.com>
2023-04-04 10:22:45 +0530

    Flask API

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -96,7 +96,8 @@ class ModelTrainer:
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
-            if best_model_score<0.7:
+
+            if best_model_score<0.6:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
 
--- src/logger.py
+++ src/logger.py
@@ -10,6 +10,8 @@ LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s]%(lineno)d %(name)s-%(levelname)s -%(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+
+
 )
