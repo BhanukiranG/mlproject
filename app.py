aa5c0294b8052d2aa70b6c4ec5d60ac9467757c2
BhanukiranG <bhanukiran5102@gmail.com>
2023-04-24 22:36:48 +0530

    final project

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def predict_datapoint():
         results=predict_pipeline.predict(pred_df)
         # print("after Prediction")
 
-        return render_template('home.html',results=results[0])
+        return render_template('index.html',results=results[0])
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",debug=True)
